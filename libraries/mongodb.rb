dfa812e7c09f56a8223cba44a99a08e6ca442d74
Edwin Mourant <e.mourant@gmail.com>
2013-03-18 20:03:50 +1300

    fix: replicaset recipe was trying to add duplicate nodes to a replica set

--- libraries/mongodb.rb
+++ libraries/mongodb.rb
@@ -45,7 +45,7 @@ class Chef::ResourceDefinitionList::MongoDB
     end
     
     # Want the node originating the connection to be included in the replicaset
-    members << node unless members.include?(node)
+    members << node if ( members.select{|member| member['fqdn'] == node['fqdn'] }.length == 0)
     members.sort!{ |x,y| x.name <=> y.name }
     rs_members = []
     members.each_index do |n|
